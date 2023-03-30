b5f26d5b52804f7e4644c174fa7eea7223051248
kleber.barilli <kleber.barilli73@gmail.com>
2023-03-30 13:55:22 -0300

    setup inicial

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,43 @@
-interface User {
-  name: string;
-}
+import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
+import pino from "pino";
 
-function saveUser(user: User) {
-  console.log(user.name);
-}
-saveUser({ name: "Kleber" });
+const logger = pino({
+  level: "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+});
+
+const server: FastifyInstance = Fastify({ logger });
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          pong: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+};
+
+server.get("/ping", opts, async (request, reply) => {
+  return { pong: "it worked!" };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+start();
